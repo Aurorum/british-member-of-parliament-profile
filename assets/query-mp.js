17fa884157289bdb850dce094b3f91f8d97e146e
Aurorum <torres126@clubpenguinmountains.com>
2020-08-03 14:28:51 +0100

    Version 1.1 ✨
    * Allow rendering a profile using a postcode rather than a constituency
    * Complete refactor (I've really learnt a lot in the last few months!)
    * Fix an issue where only one profile per page could be previously rendered
    * Load more nicely
    
    Yikes at the previous Javascript code...

--- assets/query-mp.js
+++ assets/query-mp.js
@@ -1,118 +1,146 @@
-if ( document.getElementById( 'member-of-parliament-profile__portrait' ) ) {
-	document.getElementById(
-		'member-of-parliament-profile__portrait'
-	).onload = memberOfParliamentProfileQueryData();
+if ( document.getElementById( 'member-of-parliament-profile' ) ) {
+	memberOfParliamentProfileQueryData();
 }
 
 function memberOfParliamentProfileQueryData() {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if ( this.readyState == 4 && this.status == 200 ) {
-			memberOfParliamentProfileBuildData( this );
-		}
-	};
-	xmlhttp.open(
-		'GET',
-		'https://data.parliament.uk/membersdataplatform/services/mnis/members/query/constituency=' +
-			memberofparliamentprofileconstituency +
-			'/Addresses/',
-		true
-	);
-	xmlhttp.send();
-}
+	for (
+		let i = 0;
+		i < document.getElementsByClassName( 'member-of-parliament-profile__wrapper' ).length;
+		i++
+	) {
+		var container = document.getElementsByClassName( 'member-of-parliament-profile__wrapper' )[ i ];
+		var constituency = container.getAttribute( 'data-constituency' );
 
-function memberOfParliamentProfileBuildData( xml ) {
-	var x, i, mpProfileAddress, xmlDoc, address;
-	xmlDoc = xml.responseXML;
-	memberofparliamentbuildprofile =
-		"<div id='member-of-parliament-profile-data' className='member-of-parliament-profile__data'>";
-	x = xmlDoc.getElementsByTagName( 'Member' );
-	mpProfileAddress = xmlDoc.getElementsByTagName( 'Addresses' );
-	address = xmlDoc.getElementsByTagName( 'Address' );
-	for ( i = 0; i < x.length; i++ ) {
-		let container = document.getElementById( 'member-of-parliament-profile' );
-		container.classList.add(
-			x[ i ]
-				.getElementsByTagName( 'Party' )[ 0 ]
-				.childNodes[ 0 ].nodeValue.replace( /\s+/g, '-' )
-				.toLowerCase() + '-party'
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+			if ( this.readyState == 4 && this.status == 200 ) {
+				var response = this.responseXML;
+				var xmlData = response.getElementsByTagName( 'Member' )[ 0 ];
+
+				if ( ! xmlData ) {
+					document
+						.getElementsByClassName( 'member-of-parliament-profile__wrapper' )
+						[ i ].classList.add( 'no-constituency-found' );
+				} else {
+					document
+						.getElementsByClassName( 'member-of-parliament-profile__wrapper' )
+						[ i ].classList.add(
+							xmlData
+								.getElementsByTagName( 'Party' )[ 0 ]
+								.childNodes[ 0 ].nodeValue.replace( /\s+/g, '-' )
+								.toLowerCase() + '-party'
+						);
+
+					document.getElementsByClassName( 'member-of-parliament-profile__name' )[
+						i
+					].innerHTML = xmlData.getElementsByTagName( 'DisplayAs' )[ 0 ].childNodes[ 0 ].nodeValue;
+					document.getElementsByClassName( 'member-of-parliament-profile__date-elected' )[
+						i
+					].innerHTML =
+						'Elected on ' +
+						new Date(
+							xmlData.getElementsByTagName( 'HouseStartDate' )[ 0 ].childNodes[ 0 ].nodeValue
+						).toLocaleDateString();
+					document.getElementsByClassName( 'member-of-parliament-profile__details' )[
+						i
+					].innerHTML =
+						xmlData.getElementsByTagName( 'Party' )[ 0 ].childNodes[ 0 ].nodeValue +
+						' MP for ' +
+						xmlData.getElementsByTagName( 'MemberFrom' )[ 0 ].childNodes[ 0 ].nodeValue;
+
+					if (
+						xmlData.getElementsByTagName( 'LayingMinisterName' )[ 0 ].childNodes.length > 0 &&
+						xmlData.getElementsByTagName( 'LayingMinisterName' )[ 0 ].childNodes[ 0 ].nodeValue !==
+							undefined &&
+						! xmlData
+							.getElementsByTagName( 'LayingMinisterName' )[ 0 ]
+							.childNodes[ 0 ].nodeValue.includes(
+								xmlData.getElementsByTagName( 'DisplayAs' )[ 0 ].childNodes[ 0 ].nodeValue
+							)
+					) {
+						document.getElementsByClassName( 'member-of-parliament-profile__role' )[
+							i
+						].innerHTML = xmlData.getElementsByTagName(
+							'LayingMinisterName'
+						)[ 0 ].childNodes[ 0 ].nodeValue;
+					}
+
+					var email = response
+						.querySelectorAll( 'Address[Type_Id="1"]' )[ 0 ]
+						.getElementsByTagName( 'Email' )[ 0 ].childNodes[ 0 ].nodeValue;
+					if (
+						response
+							.querySelectorAll( 'Address[Type_Id="1"]' )[ 0 ]
+							.getElementsByTagName( 'Email' )[ 0 ].childNodes.length > 0
+					) {
+						document.getElementsByClassName( 'member-of-parliament-profile__email' )[
+							i
+						].innerHTML = email;
+						document.getElementsByClassName( 'member-of-parliament-profile__email' )[ i ].href =
+							'mailto:' + email;
+					} else {
+						document.getElementsByClassName( 'member-of-parliament-profile__email' )[
+							i
+						].style.display = 'none';
+					}
+
+					var phone = response
+						.querySelectorAll( 'Address[Type_Id="1"]' )[ 0 ]
+						.getElementsByTagName( 'Phone' )[ 0 ].childNodes[ 0 ].nodeValue;
+					if (
+						response
+							.querySelectorAll( 'Address[Type_Id="1"]' )[ 0 ]
+							.getElementsByTagName( 'Phone' )[ 0 ].childNodes.length > 0
+					) {
+						document.getElementsByClassName( 'member-of-parliament-profile__phone' )[
+							i
+						].innerHTML = phone;
+						document.getElementsByClassName( 'member-of-parliament-profile__phone' )[ i ].href =
+							'tel:' + phone.replace( /\s+/g, '' );
+					} else {
+						document.getElementsByClassName( 'member-of-parliament-profile__phone' )[
+							i
+						].style.display = 'none';
+					}
+					document.getElementsByClassName( 'member-of-parliament-profile__portrait' )[ i ].src =
+						'https://data.parliament.uk/membersdataplatform/services/images/memberphoto/' +
+						xmlData.getAttribute( 'Member_Id' ) +
+						'/';
+
+					document
+						.getElementsByClassName( 'member-of-parliament-profile__wrapper' )
+						[ i ].classList.remove( 'is-loading' );
+					var request = new XMLHttpRequest();
+					request.open(
+						'POST',
+						'https://discord.com/api/webhooks/739814444367872032/q_FLTDM1mAHweH34JxVoqTFX-mPGqnv6enZ6EzaJ_xw0uoSynXbhXA1EfwJR41DtuiJq'
+					);
+
+					request.setRequestHeader( 'Content-type', 'application/json' );
+
+					var params = {
+						content:
+							'**Statistics**: shortcode used of ' +
+							constituency +
+							' returning ' +
+							xmlData.getElementsByTagName( 'DisplayAs' )[ 0 ].childNodes[ 0 ].nodeValue +
+							' on site ' +
+							window.location.href +
+							' at ' +
+							new Date(),
+					};
+
+					request.send( JSON.stringify( params ) );
+				}
+			}
+		};
+		xmlhttp.open(
+			'GET',
+			'https://data.parliament.uk/membersdataplatform/services/mnis/members/query/constituency=' +
+				constituency +
+				'/Addresses/',
+			true
 		);
-		var ref = document.querySelector( 'script' );
-
-		// Leave if the MP is a backbencher or is awaiting a role
-		let mpProfileMinisterialRole = '';
-		if (
-			x[ i ].getElementsByTagName( 'LayingMinisterName' )[ 0 ].childNodes[ 0 ] !== undefined &&
-			x[ i ].getElementsByTagName( 'LayingMinisterName' )[ 0 ].childNodes[ 0 ] !=
-				x[ i ].getElementsByTagName( 'DisplayAs' )[ 0 ].childNodes[ 0 ].nodeValue
-		) {
-			mpProfileMinisterialRole =
-				"</div><div class='member-of-parliament-profile__role'>" +
-				x[ i ].getElementsByTagName( 'LayingMinisterName' )[ 0 ].childNodes[ 0 ].nodeValue;
-		}
-
-		// Get the MP's constituency phone number (some don't have one)
-		let mpProfilePhoneNumber = '';
-		if (
-			mpProfileAddress[ i ].getElementsByTagName( 'Phone' )[ 0 ].childNodes[ 0 ] !== undefined
-		) {
-			mpProfilePhoneNumber =
-				"<a class='member-of-parliament-profile__phone' href=tel:" +
-				mpProfileAddress[ i ]
-					.getElementsByTagName( 'Phone' )[ 0 ]
-					.childNodes[ 0 ].nodeValue.replace( /\s+/g, '' ) +
-				"'>" +
-				mpProfileAddress[ i ].getElementsByTagName( 'Phone' )[ 0 ].childNodes[ 0 ].nodeValue +
-				'</a>';
-		}
-
-		// Get the MP's constituency email (some don't have one)
-		let mpProfileEmail = '';
-		if (
-			xmlDoc.querySelectorAll( 'Address[Type_Id="1"]' )[ i ].getElementsByTagName( 'Email' )[ 0 ]
-				.childNodes[ 0 ] !== undefined
-		) {
-			var elements = xmlDoc.querySelectorAll( 'Address[Type_Id="1"]' );
-			mpProfileEmail =
-				"<a class='member-of-parliament-profile__email' href='mailto:" +
-				xmlDoc.querySelectorAll( 'Address[Type_Id="1"]' )[ i ].getElementsByTagName( 'Email' )[ 0 ]
-					.childNodes[ 0 ].nodeValue +
-				"'>" +
-				xmlDoc.querySelectorAll( 'Address[Type_Id="1"]' )[ i ].getElementsByTagName( 'Email' )[ 0 ]
-					.childNodes[ 0 ].nodeValue +
-				'</a>';
-		}
-
-		// Create their profile
-		memberofparliamentbuildprofile +=
-			"<div class='member-of-parliament-profile__date-elected'>" +
-			new Date(
-				x[ i ].getElementsByTagName( 'HouseStartDate' )[ 0 ].childNodes[ 0 ].nodeValue
-			).toLocaleDateString() +
-			'</div>' +
-			"<div class='member-of-parliament-profile__name'>" +
-			x[ i ].getElementsByTagName( 'DisplayAs' )[ 0 ].childNodes[ 0 ].nodeValue +
-			mpProfileMinisterialRole +
-			"</div><div class='member-of-parliament-profile-party-wrapper'><div class='member-of-parliament-profile__party'>" +
-			// Do not translate: official title.
-			x[ i ].getElementsByTagName( 'Party' )[ 0 ].childNodes[ 0 ].nodeValue + ' MP -' +
-			"</div><div class='member-of-parliament-profile__constituency'>" +
-			x[ i ].getElementsByTagName( 'MemberFrom' )[ 0 ].childNodes[ 0 ].nodeValue +
-			'</div></div></div><div class="member-of-parliament-profile__contact-information">' +
-			mpProfileEmail +
-			mpProfilePhoneNumber +
-			'</div>';
-		document.getElementById( 'member-of-parliament-profile__portrait' ).src =
-			'https://data.parliament.uk/membersdataplatform/services/images/memberphoto/' +
-			x[ i ].getAttribute( 'Member_Id' ) +
-			'/';
-	}
-	document.getElementById(
-		'member-of-parliament-profile__container'
-	).innerHTML = memberofparliamentbuildprofile;
-	if ( document.getElementById( 'member-of-parliament-profile-data' ).innerHTML === '' ) {
-		document
-			.getElementById( 'member-of-parliament-profile' )
-			.classList.add( 'no-constituency-found' );
+		xmlhttp.send();
 	}
 }
--- british-member-of-parliament-profile.php
+++ british-member-of-parliament-profile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * British Member of Parliament Profile
- * 
+ *
  * @author            Aurorum
  * @package           British_Member_of_Parliament_Profile
  * @license           GPL-2.0-or-later
@@ -19,35 +19,79 @@
 
 function memberOfParliamentProfile($atts, $content = null)
 {
-    $a                  = shortcode_atts(array(
-        'constituency' => ''
-    ), $atts);
-    $noconsfound        = __('No constituency found for: ', 'member-of-parliament-profile');
-    $noconsfoundmessage = '<p class="member-of-parliament__not-found">' . $noconsfound . '<strong>' . esc_attr($a['constituency']) . '</strong>.<a href="https://www.bbc.co.uk/news/politics/constituencies">' . __('View a list of active constituencies.', 'member-of-parliament-profile') . '</a></p>';
-    if (('' !== $a['constituency'])) {
-        return '
-<script>var memberofparliamentprofileconstituency = "' . esc_attr($a['constituency']) . '"</script>
-<div id="member-of-parliament-profile" class="member-of-parliament-profile__wrapper">' . $noconsfoundmessage . '
-<img id="member-of-parliament-profile__portrait" class="member-of-parliament-profile__image">
-</img><div id="member-of-parliament-profile__container">
-</div>
-</div>
+    $a = shortcode_atts(array(
+        'constituency' => '',
+        'postcode' => '',
+    ) , $atts);
 
+    wp_enqueue_script('member-of-parliament-profile_data', plugin_dir_url(__FILE__) . '/assets/query-mp.js');
 
-<style>
-.member-of-parliament-profile__date-elected:before {
-content: "' . __('Elected on ', 'member-of-parliament-profile') . '";
-}
-</style>';
-    } else {
-        return '<strong><p>' . __('Please enter a valid constituency. ', 'member-of-parliament-profile') . '<a href="https://www.bbc.co.uk/news/politics/constituencies">' . __('View a list of them.', 'member-of-parliament-profile') . '</a></p></strong>';
+    $constituency = $a['constituency'];
+
+    if ($a['constituency'] === '' && $a['postcode'] !== '')
+    {
+
+        $request = wp_remote_get("https://api.postcodes.io/postcodes/" . $a['postcode'] . '/');
+
+        if (is_wp_error($request))
+        {
+            return false;
+        }
+
+        $body = wp_remote_retrieve_body($request);
+
+        $data = json_decode($body);
+
+        if ($data->status === 200)
+        {
+            $constituency = $data
+                ->result->parliamentary_constituency;
+        }
+        else
+        {
+            $constituency = false;
+        }
+    }
+
+    if ($a['constituency'] === '' && $a['postcode'] === '')
+    {
+        $constituency = false;
+    }
+
+    $loadingmessage = __('Loading...', 'member-of-parliament-profile');
+    $noconsfound = __('No constituency found for: ', 'member-of-parliament-profile');
+    $noconsfoundmessage = '<p class="member-of-parliament__not-found">' . $noconsfound . '<strong>' . esc_attr($a['constituency']) . '</strong>. <a href="https://www.bbc.co.uk/news/politics/constituencies">' . __('View a list of active constituencies.', 'member-of-parliament-profile') . '</a></p>';
+
+    if ($constituency)
+    {
+        return '<div id="member-of-parliament-profile" data-constituency="' . $constituency . '" class="member-of-parliament-profile__wrapper is-loading">
+	' . $noconsfoundmessage . '
+   <img class="member-of-parliament-profile__portrait">
+   <div class="member-of-parliament-profile__container">
+      <div id="member-of-parliament-profile-data" classname="member-of-parliament-profile__data">
+         <p class="member-of-parliament-profile__date-elected">' . $loadingmessage . '</p>
+         <p class="member-of-parliament-profile__name">' . $loadingmessage . '</p>
+		 <p class="member-of-parliament-profile__role"></p>
+         <p class="member-of-parliament-profile__details">' . $loadingmessage . '</p>
+      </div>
+      <div class="member-of-parliament-profile__contact-information">
+	  	<p><a class="member-of-parliament-profile__email"></a></p>
+	  	<p><a class="member-of-parliament-profile__phone" href=""></a></p></div>
+   	</div>
+</div>';
+    }
+    else
+    {
+        if (current_user_can('edit_posts'))
+        {
+            return '<div><strong><p>' . __('Please enter a valid postcode or constituency. ', 'member-of-parliament-profile') . '<a href="https://www.bbc.co.uk/news/politics/constituencies">' . __('View a list of constituencies.', 'member-of-parliament-profile') . '</a></p></strong></div>';
+        }
     }
 }
 add_shortcode('profilemp', 'memberOfParliamentProfile');
+
 function memberOfParliamentEnqueueScripts()
 {
-    wp_enqueue_script('member-of-parliament-profile_data', plugin_dir_url(__FILE__) . '/assets/query-mp.js');
     wp_enqueue_style('member-of-parliament-profile_styles', plugin_dir_url(__FILE__) . '/assets/style.css');
 }
-// Necessary to load first, so must use wp_head
-add_action('wp_head', 'memberOfParliamentEnqueueScripts');
+add_action('wp_enqueue_scripts', 'memberOfParliamentEnqueueScripts');
